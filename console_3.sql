ec511ac2cdaf751d243678a27571cc0082618f66
Abdul24 <abdulseyed24@gmail.com>
2021-10-28 13:45:33 -0400

    Window function sql customer and order tables

---
+++ console_3.sql
@@ -0,0 +1,10 @@
+SELECT province, city, first_name, last_name, total
+FROM (SELECT c.PROVINCE, c.CITY, c.first_name, c.last_name, Min(o.total_price) as total,
+             ROW_NUMBER() OVER (PARTITION BY c.province ORDER BY Max(o.total_price)) as amount
+      FROM orders o INNER JOIN
+           customers c
+           ON o.customer_id= c.customer_id
+      GROUP BY c.PROVINCE, c.CITY, c.last_name, c.first_name
+     ) sc
+WHERE amount = 1
+ORDER BY total DESC;
